3b139829b1662cc387c6b5edcda3ef91dfb945ef
NicoleChen <CCChenfei@outlook.com>
2017-10-10 13:44:33 +0800

    crop by bounding box

--- crop_img.m
+++ crop_img.m
@@ -11,27 +11,61 @@ for i = [1,5,6,7,8,9,11]
             load(['/home/chenf/Documents/pose_estimation/data/human3.6M_ori/S',num2str(i),'/MySegmentsMat/ground_truth_bb/',num2str(i),num2str(j),num2str(k),'.mat'])
             for m = 1:n
                 I = imread(['S',num2str(i),'/c',num2str(k),'_',num2str(j),'/im',num2str(m),'.jpg']);
+%                 imshow(I);
+                
+%                 for h = 1:32
+%                     plot(gt(h,1,1),gt(h,2,1),'r.');
+%                     hold on;
+%                 end
+                
+                
                 mask = Masks{m};
                 [max_x,max_y] = find(mask==1);
-                top_loc = [max_x(1) max_y(1)];
-                [I_x, I_y, I_z]=size(I);
-                Mask = zeros(I_x, I_y, 3);
-                for i = 1:3
-                    Mask(:,:,i) = double(mask);
+                top_loc = [max_y(1) max_x(1)];
+                [num, num_] = size(max_x);
+                bottom_loc = [max_y(num) max_x(num) ];
+%                 [I_x, I_y, I_z]=size(I);
+%                 Mask = zeros(I_x, I_y, 3);
+%                 for i = 1:3
+%                     Mask(:,:,i) = double(mask);
+%                 end
+%                 I = Mask.*double(I);
+%                 I(all(I==0,2),:,:)=[];
+%                 I(:,:,all(I==0,1))=[];
+                pad_x = bottom_loc(1)-top_loc(1);
+                pad_y = bottom_loc(2)-top_loc(2);
+                I = imcrop(I,[top_loc(1) top_loc(2) pad_x pad_y]);
+                if pad_x>pad_y
+                    if rem(pad_x-pad_y,2)==0
+                        I = padarray(I,[(pad_x-pad_y)/2 0]);
+                    else
+                        I = padarray(I,[fix((pad_x-pad_y)/2) 0],'pre');
+                        I = padarray(I,[fix((pad_x-pad_y)/2)+1 0],'post');
+                    end
+                    [row,col,dim] = size(I);
+                    for t = 1:t1
+                        gt_new(t,1,m) = (gt(t,1,m)-top_loc(1))*256/row;
+                        gt_new(t,2,m) = (gt(t,2,m)-top_loc(2)+fix((pad_x-pad_y)/2))*256/col;
+                    end
+                else
+                    if rem(pad_y-pad_x,2)==0
+                        I = padarray(I,[0 (pad_y-pad_x)/2]);
+                    else
+                        I = padarray(I,[0 fix((pad_y-pad_x)/2)],'pre');
+                        I = padarray(I,[0 fix((pad_y-pad_x)/2)+1],'post');
+                    end
+                    [row,col,dim] = size(I);
+                    for t = 1:t1
+                        gt_new(t,1,m) = (gt(t,1,m)-top_loc(1)+fix((pad_y-pad_x)/2))*256/row;
+                        gt_new(t,2,m) = (gt(t,2,m)-top_loc(2))*256/col;
+                    end
                 end
-                I = Mask.*double(I);
-                I(all(I==0,2),:,:)=[];
-                I(:,:,all(I==0,1))=[];
-                [row,col] = size(I);
-                %I = imcrop(I,[gt(1,1,m)-256 gt(1,2,m)-256 512 512]);
+ 
                 I = imresize(I,[256 256]);
+%                 figure;imshow(I);
                 mkdir(['../Human3.6Mall/s',num2str(i),'/c',num2str(k),'_',num2str(j)])
                 imwrite(I,['../Human3.6Mall/s',num2str(i),'/c',num2str(k),'_',num2str(j),'/im',num2str(m),'.jpg']);
                 
-                for t = 1:t1
-                    gt_new(t,1,m) = (gt(t,1,m)-max_x)*256/row;
-                    gt_new(t,2,m) = (gt(t,2,m)-max_y)*256/col;
-                end
             end
             save(['../Human3.6Mall/s',num2str(i),'/c',num2str(k),'_',num2str(j),'/joints.mat'],'gt_new');
         end
